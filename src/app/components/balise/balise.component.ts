a5afc190ab5b771e6ce07be8e703f4a496a46f26
RemiDev <remidev@DevRemi.home>
2018-06-09 01:38:19 +0200

    Ajout carousel IMG Media- Balise

--- src/app/components/balise/balise.component.ts
+++ src/app/components/balise/balise.component.ts
@@ -1,18 +1,31 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 import { Balise } from '../../class/balise';
 import { Globals } from '../../class/globals';
+import { Media } from '../../class/media';
 
 @Component({
   selector: 'balise-component',
   templateUrl: './balise.component.html',
   styleUrls: ['./balise.component.scss']
 })
-export class BaliseComponent {
+export class BaliseComponent implements OnChanges{
   @Input() balise: Balise;
   @Output() openModal: EventEmitter<any> = new EventEmitter();
 
-  constructor(private globals: Globals){
+  public currentMedia:Media = null;
 
+  constructor(private globals: Globals){
+    if(this.balise && this.balise.Medias){
+      this.currentMedia = this.balise.Medias[0];
+    }
+    console.log(this.currentMedia)
+  }
+
+  ngOnChanges():void{
+    if(this.balise && this.balise.Medias && this.balise.Medias.length > 0){
+      this.currentMedia = this.balise.Medias[0];
+      console.log(this.currentMedia.Link)
+    }
   }
 
   public openModifModal():void{
